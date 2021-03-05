611c3f4756e4abe2160579ec1b8d224868977e01
Marcos Dornellas <mvdornellas@stefanini.com>
2021-03-04 23:41:52 -0300

    compliance rating

--- src/app/company/services/company.service.ts
+++ src/app/company/services/company.service.ts
@@ -5,13 +5,14 @@ export type CompanyModel = {
     name: string,
     email: string,
     cnpj: string,
-    standards: [{
+    standards: Array<{
         id: string,
-        name: string
-    }]
+        name: string,
+        rating?: number
+    }>
     startHire: string,
     endHire: string,
-    rating: number
+    token?: string
 };
 
 @Injectable()
@@ -28,9 +29,35 @@ export class CompanyService {
             email: 'dornellas13@gmail.com',
             endHire: '7689732',
             startHire: '2828923',
-            standards: [{id: '', name: '121'}],
-            rating: 1
-
+            standards: [{id: 'NR-1', name: 'TESTE NR-1', rating: 10 }, {id: 'NR-2', name: 'TESTE NR-2', rating: 3 }, {id: 'NR-3', name: 'TESTE NR-3', rating: 5 }, {id: 'NR-5', name: 'TESTE NR-5', rating: 10 }]
+        }, {
+            id: 1,
+            name: 'Marcos teste',
+            cnpj: 'jh83272983',
+            email: 'dornellas13@gmail.com',
+            endHire: '7689732',
+            startHire: '2828923',
+            standards: [{id: 'NR-1', name: 'TESTE NR-1', rating: 10 }, {id: 'NR-2', name: 'TESTE NR-2', rating: 3 }, {id: 'NR-3', name: 'TESTE NR-3', rating: 5 }, {id: 'NR-5', name: 'TESTE NR-5', rating: 10 }]
         }];
+    }
+
+    get(id: number): CompanyModel {
+        return {
+            id: 1,
+            name: 'Marcos teste',
+            cnpj: 'jh83272983',
+            email: 'dornellas13@gmail.com',
+            endHire: '7689732',
+            startHire: '2828923',
+            standards: [{id: 'NR-1', name: 'TESTE NR-1'  }, {id: 'NR-2', name: 'TESTE NR-2' }],
+        };
+    }
+
+    update(company: CompanyModel): boolean {
+        return true;
+    }
+
+    checkComplianceAuthorization(companyId, token): boolean {
+        return true;
     }
 }
